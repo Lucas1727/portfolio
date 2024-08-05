8a1b569dd4b5e2479548144069baac8e366341bf
Lucas Ridge <lucasridge1727@gmail.com>
2024-08-05 19:20:13 +0100

    Able to tweak background icons

--- assets/ts/methods.ts
+++ assets/ts/methods.ts
@@ -108,3 +108,15 @@ export const createHeaders = function (headers?: Record<string, string | null>[]
     return object = {...object}
   }
 }
+
+export const shuffleArray = function (array: []) {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+
+    const temp = array[i];
+    array[i] = array[j];
+    array[j] = temp;
+  }
+
+  return array;
+}
--- pages/index.vue
+++ pages/index.vue
@@ -6,7 +6,7 @@
     <v-img v-if="zoom" v-for="item in currentBackgrounds" :src="item" height="256" width="256" class="zoom"></v-img>
 
     <div style="padding-top: 2em;">
-      <div id="cv-app" class="app" @click="">
+      <div id="cv-app" class="app" @click="openCV">
         <div class=app-label>CV</div>
       </div>
 
@@ -30,21 +30,26 @@
 
       <div id="settings-body" class="window-body">
         <h1>Settings</h1>
-        <h2 style="padding-top: 1em">Wallpaper</h2>
-        <h3 style="padding-top: 1em">Animation style</h3>
+        <h2>Wallpaper</h2>
+        <h3>Animation style</h3>
         <v-btn class="v-btn" @click="toggleFly">Fly</v-btn>
         <v-btn class="v-btn" @click="toggleZoom">Zoom</v-btn>
-        <h3 style="padding-top: 1em">Icon select</h3>
+        <h3>Icon select</h3>
         <div class="icon-select">
-          <v-img v-for="item in apps" :key="item" class="icon" :src="item.image" @click="">
-            <v-tooltip activator="parent" location="bottom" open-delay="500">{{ item.name }}</v-tooltip>
+          <v-img v-for="app in apps" :key="app" class="icon" :src="app.image" @click="toggleBackgroundIcon(app.image)">
+            <v-tooltip activator="parent" location="bottom" open-delay="500">{{ app.name }}</v-tooltip>
             <template v-slot:placeholder>
               <div class="d-flex align-center justify-center fill-height">
                 <v-progress-circular color="white" indeterminate></v-progress-circular>
               </div>
             </template>
+            <v-icon v-if="backgrounds.includes(app.image)" class=label style="color: green">mdi-check</v-icon>
+            <v-icon v-else class=label style="color: red">mdi-check</v-icon>
           </v-img>
         </div>
+        <v-btn class="v-btn" @click="populateBackground" style="margin-top: 1em">Randomize</v-btn>
+        <v-divider class="v-divider"></v-divider>
+        <h2>System</h2>
       </div>
     </div>
 
@@ -81,7 +86,7 @@
 </template>
 
 <script>
-import {bringToFront, centerElement, dragElement, openElements, resetElement} from "assets/ts/methods.ts";
+import {bringToFront, centerElement, dragElement, openElements, resetElement, shuffleArray} from "assets/ts/methods.ts";
 
 export default {
   setup() {
@@ -89,11 +94,24 @@ export default {
   data() {
     return {
       apps: [
-        {id: 'kotlin', name: 'Kotlin', image: 'kotlin.svg', height: '2.6em', bottom: '2em'},
+        {id: 'apple', name: 'Apple', image: 'apple.svg', height: '3em', bottom: '1.6em'},
+        {id: 'cv', name: 'CV', image: 'word.svg', height: '3em', bottom: '1.8em'},
+        {id: 'css', name: 'CSS', image: 'css.svg', height: '3em', bottom: '1.4em'},
+        {id: 'docker', name: 'Docker', image: 'docker.svg', height: '3em', bottom: '1.4em'},
+        {id: 'github', name: 'GitHub', image: 'github.svg', height: '3em', bottom: '1.4em'},
+        {id: 'gitlab', name: 'GitLab', image: 'gitlab.svg', height: '3em', bottom: '1.4em'},
+        {id: 'html', name: 'HTML', image: 'html.svg', height: '3em', bottom: '1.4em'},
         {id: 'java', name: 'Java', image: 'java.svg', height: '3em', bottom: '2.2em'},
+        {id: 'javascript', name: 'JavaScript', image: 'javascript.svg', height: '3em', bottom: '1.4em'},
+        {id: 'linkedin', name: 'LinkedIn', image: 'linkedin.svg', height: '3em', bottom: '1.4em'},
+        {id: 'kotlin', name: 'Kotlin', image: 'kotlin.svg', height: '2.6em', bottom: '2em'},
         {id: 'mysql', name: 'MySQL', image: 'mysql.svg', height: '3em', bottom: '1.2em'},
+        {id: 'linux', name: 'Linux', image: 'linux.svg', height: '3em', bottom: '1.4em'},
+        {id: 'nuxt', name: 'Nuxt', image: 'nuxt.svg', height: '3em', bottom: '1.4em'},
+        {id: 'postgres', name: 'Postgres', image: 'postgres.svg', height: '3em', bottom: '1.4em'},
         {id: 'terminal', name: 'Terminal', image: 'terminal.svg', height: '3em', bottom: '1.4em'},
         {id: 'settings', name: 'Settings', image: 'settings.svg', height: '3em', bottom: '1.6em'},
+        {id: 'windows', name: 'Windows', image: 'windows.svg', height: '3em', bottom: '1.4em'},
       ],
       openApps: [],
       backgrounds: [],
@@ -134,9 +152,12 @@ export default {
       openElements.push(document.getElementById('terminal'));
       this.openApps.push('terminal');
     },
+    openCV() {
+      window.open('/cv.pdf');
+    },
     openWindow(app) {
       const element = document.getElementById(app);
-      if (!openElements.includes(element)) {
+      if (!openElements.includes(element) && !this.openApps.includes(app)) {
         openElements.push(element);
         this.openApps.push(app);
         bringToFront(element);
@@ -144,7 +165,7 @@ export default {
       }
     },
     populateBackground() {
-      this.backgrounds = this.apps.map(item => item.image);
+      this.backgrounds = shuffleArray(this.apps.map(item => item.image)).slice(0, 5);
     },
     sizeWindow(app) {
       const element = document.getElementById(app);
@@ -170,6 +191,13 @@ export default {
       this.fly = true;
       this.zoom = false;
     },
+    toggleBackgroundIcon(app) {
+      if (this.backgrounds.includes(app)) {
+        this.backgrounds = this.backgrounds.filter(item => item !== app);
+      } else {
+        this.backgrounds.push(app);
+      }
+    },
     toggleWindow(app) {
       const element = document.getElementById(app);
       if (openElements.includes(element)) {
@@ -187,6 +215,6 @@ export default {
     this.loadElements();
     this.populateBackground();
     this.ready = true;
-  }
+  },
 }
 </script>
