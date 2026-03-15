10eda40c00d1bc36d722198aaecfc71cb1187ab9
Lucas Ridge <lucasridge1727@gmail.com>
2026-03-15 18:16:00 +0000

    Print version

--- app/app.vue
+++ app/app.vue
@@ -5,8 +5,15 @@
 </template>
 
 <script setup lang="ts">
+const config = useRuntimeConfig();
+
 useHead({
   title: 'Lucas Ridge - Full-Stack Developer',
   meta: [{name: 'Lucas Ridge - Full-Stack Developer'}],
+})
+
+onMounted(() => {
+  console.log(`Build Hash: ${config.public.buildHash}`)
+  console.log(`Build Id: ${config.public.buildId}`)
 })
 </script>
