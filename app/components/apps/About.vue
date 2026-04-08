33df24317f19eb8cfbe2b5663ccf4154669847e4
Lucas Ridge <lucasridge1727@gmail.com>
2026-04-08 12:25:52 +0100

    Some updates to the about component

--- app/components/apps/About.vue
+++ app/components/apps/About.vue
@@ -46,25 +46,29 @@
 
       <USeparator/>
 
-      <!--      <UMarquee>-->
-      <!--        <UIcon name="i-simple-icons-github" class="size-10 shrink-0"/>-->
-      <!--        <UIcon name="i-simple-icons-discord" class="size-10 shrink-0"/>-->
-      <!--        <UIcon name="i-simple-icons-x" class="size-10 shrink-0"/>-->
-      <!--        <UIcon name="i-simple-icons-instagram" class="size-10 shrink-0"/>-->
-      <!--        <UIcon name="i-simple-icons-linkedin" class="size-10 shrink-0"/>-->
-      <!--        <UIcon name="i-simple-icons-facebook" class="size-10 shrink-0"/>-->
-      <!--      </UMarquee>-->
-
       <p class="flex items-center gap-2 font-bold text-[12px] text-(--gnome-text-dim) uppercase">
         <UIcon name="mdi:code" class="size-4"/>
         Work I've done
       </p>
-      <div class="grid grid-cols-2 gap-2">
-        <UButton v-for="item in workIveDone" target="_blank" variant="ghost" color="neutral" class="flex flex-col p-2! text-center text-(--gnome-panel-2) bg-(--gnome-text) hover:bg-(--gnome-text-bright) active:bg-(--gnome-text-bright) " :to="item.href">
-          <p class="text-[14px] font-bold">{{ item.title }}</p>
-          <p class="text-[12px]">{{ item.desc }}</p>
+      <div class="grid grid-cols-1 gap-2">
+        <UButton v-for="item in workIveDone" target="_blank" color="neutral" class="relative flex flex-col px-16! py-12! text-center text-(--gnome-text) overflow-hidden" :to="item.href">
+          <div class="absolute inset-0 bg-cover bg-center" :style="{ backgroundImage: `url(${item.background})` }"/>
+          <div class="absolute inset-0 bg-black/80 transition-all duration-200 hover:bg-black/90"/>
+
+          <p class="relative z-10 text-[14px] font-bold">{{ item.title }}</p>
+          <p class="relative z-10 text-[12px]">{{ item.desc }}</p>
         </UButton>
       </div>
+
+      <USeparator/>
+
+      <p class="flex items-center gap-2 font-bold text-[12px] text-(--gnome-text-dim) uppercase">
+        <UIcon name="tdesign:app" class="size-4"/>
+        What I use
+      </p>
+      <UMarquee :overlay="false">
+        <UIcon v-for="icon in whatIUse" :name="icon" class="size-10"/>
+      </UMarquee>
     </div>
   </div>
 </template>
@@ -84,7 +88,14 @@ function getStyle() {
   return `background: color-mix(in srgb, var(${color}) 50%, white)`
 }
 
+const whatIUse = [
+  'logos:vue', 'logos:nuxt-icon', 'logos:typescript-icon', 'devicon:tailwindcss',
+  'logos:kotlin-icon', 'logos:python', 'logos:postgresql', 'devicon:csharp',
+  'logos:docker-icon', 'logos:aws', 'logos:mysql', 'logos:electron', 'logos:unity',
+]
+
 const links = [
+  {icon: 'i-simple-icons-github', label: 'GitHub', href: 'https://github.com/Lucas1727'},
   {icon: 'i-simple-icons-linkedin', label: 'LinkedIn', href: 'https://www.linkedin.com/in/lucas-ridge-08964942'},
   {icon: 'i-heroicons-envelope', label: 'Email', href: 'mailto:lucasridge1727@gmail.com'},
   {icon: 'mi:document', label: 'CV', href: 'https://docs.google.com/document/d/e/2PACX-1vTiNF5gQ9DOkbNGuza8HH6rEN-lO4MsLg5K8x8rwFWx9wKzcDxjUHnIpxuoVpEpaAzDNPMxfx-ZpW-2/pub'}
@@ -100,26 +111,32 @@ const workIveDone = [
   {
     title: 'vTime Website',
     href: 'https://vtime.net',
-    background: getStyle(),
+    background: 'https://static.vtime.net/website/xr_1_mq.jpg',
     desc: 'Updated the vTime Website to use NUXT 3, a new CMS system and UI for content along with maintaining and upgrading the websites API that connects to multiple services.'
   },
   {
     title: 'vTime Games',
     href: 'https://vtimegames.com',
-    background: getStyle(),
+    background: 'https://vtimegames.com/_nuxt/vTime_Games_Logo_Horizontal.B95hJBiW.png',
     desc: 'Co-created the webpage for vTime Games, as well as creating all the SEO, dynamic scaling and optimisations for the webpage'
   },
   {
     title: 'Reality+ Website',
     href: 'https://www.realityplus.vtime.net',
-    background: getStyle(),
+    background: 'https://play-lh.googleusercontent.com/G4sc3Us-oo-6-I4TUAO4l_-igJFiS8BSZ3BX7WnlypWCeda0niX-e26-rtJeF0Q1ODvR=w1052-h592-rw',
     desc: 'Worked on the website and API that connects together with the Reality+ app.'
   },
   {
     title: 'Border Bots VR',
     href: 'https://vtimegames.com/our-games/border-bots-vr',
-    background: getStyle(),
-    desc: 'Created internal tools for the development of Border Bots VR, like a custom audio management system during the making of the game.'
+    background: 'https://static.vtime.net/website_games/border_bots/bb_final_vertical_logo_mq.png',
+    desc: 'Created internal tools for the development of Border Bots VR, including a custom audio management system for easy organisation and integration of audio into the game engine.'
+  },
+  {
+    title: 'Unreleased Team17 Game',
+    href: 'https://vtime.net/news/2024/team17-partners-with-vtime-games-for-unannounced-title',
+    background: 'https://directus.vtime.net/assets/a01fdf4b-fb57-46ae-979d-b653dfe6c36c',
+    desc: 'Currently working on an unannounced title in partnership with Team17. Working on the UI system for the games engine and creation of Unity Cloud Code for core game functionality. Also creating QA and admin tools via electron for each role out to modify core game elements and cloud functionality'
   }
 ]
 </script>
